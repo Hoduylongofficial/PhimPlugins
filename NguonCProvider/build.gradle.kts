27d7b1d5ba40b1f2e0c9f38eca22acbe24d99c15
Hoduylongofficial <123788346+Hoduylongofficial@users.noreply.github.com>
2026-05-11 13:09:34 +0700

    Update build.gradle.kts

--- NguonCProvider/build.gradle.kts
+++ NguonCProvider/build.gradle.kts
@@ -5,6 +5,6 @@ cloudstream {
     description = "Xem phim từ NguonC - HD Vietsub"
     authors     = listOf("Hoduylongofficial")
     status      = 1
-    tvTypes     = listOf(TvType.Movie, TvType.TvSeries, TvType.Anime)
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
     iconUrl     = "https://phim.nguonc.com/favicon.ico"
 }
