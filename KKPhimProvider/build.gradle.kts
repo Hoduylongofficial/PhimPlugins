8201ec1404605cc693dfeb290d1f40ec3b4a1fa0
Hoduylongofficial <123788346+Hoduylongofficial@users.noreply.github.com>
2026-05-11 12:58:05 +0700

    Update build.gradle.kts

--- KKPhimProvider/build.gradle.kts
+++ KKPhimProvider/build.gradle.kts
@@ -2,9 +2,9 @@ version = 1
 
 cloudstream {
     language    = "vi"
-    description = "Xem phim từ KKPhim - HD Vietsub"   // ← đổi
+    description = "Xem phim từ KKPhim - HD Vietsub"
     authors     = listOf("Hoduylongofficial")
     status      = 1
     tvTypes     = listOf(TvType.Movie, TvType.TvSeries, TvType.Anime)
-    iconUrl     = "https://phimapi.com/favicon.ico"    // ← đổi icon
+    iconUrl     = "https://phimapi.com/favicon.ico"
 }
